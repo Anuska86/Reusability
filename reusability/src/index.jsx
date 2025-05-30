58c7d45c268844397de2ecc2b113a30f9573683e
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-05-30 12:17:39 +0200

    Theme

--- reusability/src/index.jsx
+++ reusability/src/index.jsx
@@ -7,24 +7,32 @@ import Menu from "./Components/Menu/Menu.jsx";
 import MenuButton from "./Components/Menu/MenuButton.jsx";
 import MenuDropdown from "./Components/Menu/MenuDropdown.jsx";
 import MenuItem from "./Components/Menu/MenuItem.jsx";
+import { ThemeContext, ThemeProvider } from "./Components/Button/ThemeContext.jsx";
 
 function App() {
   const videogames = ["Action", "Adventure", "Puzzle", "Strategy", "RPG"];
+  const { theme } = React.useContext(ThemeContext);
 
   return (
-    <Menu>
-      <MenuButton>Videogames</MenuButton>
-      <MenuDropdown>
-        {videogames.map((game, index) => (
-          <MenuItem key={index}>
-            <Button variant="secondary" size="sm">
-              {game}
-            </Button>
-          </MenuItem>
-        ))}
-      </MenuDropdown>
-    </Menu>
+    <div className={`app-container ${theme}`}>
+      <Menu>
+        <MenuButton>Videogames</MenuButton>
+        <MenuDropdown>
+          {videogames.map((game, index) => (
+            <MenuItem key={index}>
+              <Button variant="secondary" size="sm">
+                {game}
+              </Button>
+            </MenuItem>
+          ))}
+        </MenuDropdown>
+      </Menu>
+    </div>
   );
-}
+} 
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
