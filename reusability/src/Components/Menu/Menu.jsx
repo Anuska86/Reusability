47c9fc9b4c842cabc21aebc6a56fd95da86b776c
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-05 16:46:19 +0200

    Menu composing

--- reusability/src/Components/Menu/Menu.jsx
+++ reusability/src/Components/Menu/Menu.jsx
@@ -3,8 +3,10 @@ import Toggle from "../Toggle/ToggleIndex";
 
 export default function Menu({ children }) {
   return (
-    <div className="menu" role="menu">
-      {children}
-    </div>
+    <Toggle>
+      <div className="menu" role="menu">
+        {children}
+      </div>
+    </Toggle>
   );
 }
--- reusability/src/index.jsx
+++ reusability/src/index.jsx
@@ -7,7 +7,7 @@ import ThemeButton from "./Components/Menu/ThemeButton.jsx";
 import { ThemeProvider, ThemeContext } from "./Components/Menu/ThemeButton.jsx";
 import Menu from "./Components/Menu/MenuIndex.jsx";
 import Toggle from "./Components/Toggle/ToggleIndex.jsx";
-import { BsStar, BsStarFill } from "react-icons/bs";
+import Star from "./Components/Star.jsx";
 
 function App() {
   const { theme } = React.useContext(ThemeContext);
@@ -15,35 +15,24 @@ function App() {
   return (
     <div className={`app-container ${theme}`}>
       <div className="star">
-        <Toggle>
-          <Toggle.Button>
-            <Toggle.On>
-              <BsStarFill className="star filled" />
-            </Toggle.On>
-            <Toggle.Off>
-              <BsStar className="star" />
-            </Toggle.Off>
-          </Toggle.Button>
-        </Toggle>
+        <Star />
       </div>
       <br />
 
       <div className="content">
-        <Toggle>
-          <Menu>
-            <Toggle.Button>
-              <Menu.Button>Menu</Menu.Button>
-            </Toggle.Button>
-            <Toggle.On>
-              <Menu.Dropdown>
-                <Menu.Item>Home</Menu.Item>
-                <Menu.Item>About</Menu.Item>
-                <Menu.Item>Contact</Menu.Item>
-                <Menu.Item>Blog</Menu.Item>
-              </Menu.Dropdown>
-            </Toggle.On>
-          </Menu>
-        </Toggle>
+        <Menu>
+          <Toggle.Button>
+            <Menu.Button>Menu</Menu.Button>
+          </Toggle.Button>
+          <Toggle.On>
+            <Menu.Dropdown>
+              <Menu.Item>Home</Menu.Item>
+              <Menu.Item>About</Menu.Item>
+              <Menu.Item>Contact</Menu.Item>
+              <Menu.Item>Blog</Menu.Item>
+            </Menu.Dropdown>
+          </Toggle.On>
+        </Menu>
       </div>
       <ThemeButton></ThemeButton>
     </div>
